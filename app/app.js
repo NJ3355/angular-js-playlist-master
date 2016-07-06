be28bab30d5f5d1f05864dfd53bebd4c9b105edb
NJ3355 <pyromania0005@yahoo.com>
2016-07-06 19:28:50 -0400

    Added search, add user, delete user, and thumbnail features

--- app/app.js
+++ app/app.js
@@ -2,23 +2,46 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 
 myNinjaApp.controller('NinjaController', function($scope){
 
+	$scope.removeNinja = function(ninja){
+		var removeNinja = $scope.ninjas.indexOf(ninja);
+		$scope.ninjas.splice(removeNinja, 1);
+	};
 
+	$scope.addNinja = function() {
+		$scope.ninjas.push({
+			name: $scope.newninja.name,
+			belt: $scope.newninja.belt,
+			rate: parseInt($scope.newninja.rate),
+			available: true
+		});
+
+		$scope.newninja.name = '';
+		$scope.newninja.belt = '';
+		$scope.newninja.rate = '';
+
+	};
 
 	$scope.ninjas = [
 	{
 		name: "yoshi",
 		belt: "green",
-		rate: 50
+		rate: 50,
+		available: true,
+		thumb: "../content/images/yoshi.png"
 	},
 	{
 		name: "crystal",
 		belt: "yellow",
-		rate: 30
+		rate: 30,
+		available: true,
+		thumb: "../content/images/mario.png"
 	},
 	{
 		name: "ryu",
 		belt: "black",
-		rate: 10
+		rate: 10,
+		available: true,
+		thumb: "../content/images/ryu.png"
 	},
 	
 	
